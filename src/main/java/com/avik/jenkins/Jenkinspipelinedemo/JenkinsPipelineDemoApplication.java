546dee02807f07b4f32842d604ff51cb04c64f87
Avik Chakraborty <avik.chakraborty@lexmark.com>
2018-09-03 16:02:21 +0530

    Initial commit

---
+++ src/main/java/com/avik/jenkins/Jenkinspipelinedemo/JenkinsPipelineDemoApplication.java
@@ -0,0 +1,12 @@
+package com.avik.jenkins.Jenkinspipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineDemoApplication.class, args);
+	}
+}
